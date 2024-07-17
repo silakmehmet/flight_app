a238fcc544289ff8af663b49867fb54504139a50
silakmehmet <silakmehmet@gmail.com>
2024-07-17 11:15:18 +0300

    Serializers and views created for users

---
+++ users/serializers.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, validators=[UniqueValidator])
+    password2 = serializers.CharField(
+        write_only=True, required=True, validators=[UniqueValidator])
+    email = serializers.EmailField(required=True, validators=[
+                                   UniqueValidator(queryset=User.objects.all())])
+
+    class Meta:
+        model = User
+        fields = ["id", "email", "username", "password", "password2"]
+        read_only_fields = ["id"]
+
+        def validate(self, data):
+            if data["password"] != data["password2"]:
+                raise serializers.ValidationError({
+                    "detail": "Password fields did not match"
+                })
+
+            return data
+
+        def create(self, validated_data):
+            validated_data.pop("password2")
+
+            user = User.objects.create(**validated_data)
+            user.set_password(validated_data["password"])
+            user.save()
+
+            return user
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 
+from .views import RegisterCreateAPIView
+
 urlpatterns = [
-    path('auth/', include('dj_rest_auth.urls'))
+    path('auth/', include('dj_rest_auth.urls')),
+    path('register/', RegisterCreateAPIView.as_view()),
 
 ]

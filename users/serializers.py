f2e813bfd47ac01979aac41132e2c5ff262e240e
silakmehmet <silakmehmet@gmail.com>
2024-07-17 11:56:17 +0300

    Serializers modified

--- users/serializers.py
+++ users/serializers.py
@@ -7,9 +7,9 @@ from rest_framework.validators import UniqueValidator
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
-        write_only=True, validators=[UniqueValidator])
+        write_only=True, validators=[validate_password])
     password2 = serializers.CharField(
-        write_only=True, required=True, validators=[UniqueValidator])
+        write_only=True, required=True, validators=[validate_password])
     email = serializers.EmailField(required=True, validators=[
                                    UniqueValidator(queryset=User.objects.all())])
 
@@ -18,19 +18,18 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ["id", "email", "username", "password", "password2"]
         read_only_fields = ["id"]
 
-        def validate(self, data):
-            if data["password"] != data["password2"]:
-                raise serializers.ValidationError({
-                    "detail": "Password fields did not match"
-                })
+    def validate(self, data):
+        if data["password"] != data["password2"]:
+            raise serializers.ValidationError({
+                "detail": "Password fields did not match"
+            })
 
-            return data
+        return data
 
-        def create(self, validated_data):
-            validated_data.pop("password2")
+    def create(self, validated_data):
+        validated_data.pop("password2")
 
-            user = User.objects.create(**validated_data)
-            user.set_password(validated_data["password"])
-            user.save()
-
-            return user
+        user = User.objects.create(**validated_data)
+        user.set_password(validated_data["password"])
+        user.save()
+        return user

3090a833cbefb9509435b00ed6bd4313c8748a5c
silakmehmet <silakmehmet@gmail.com>
2024-07-17 20:10:14 +0300

    Filtering added for flights

--- flight_app/views.py
+++ flight_app/views.py
@@ -1,3 +1,5 @@
+from datetime import date, datetime
+
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
@@ -10,6 +12,23 @@ class FlightMVS(ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
     permission_classes = [IsAdminOrReadOnly]
+
+    def get_queryset(self):
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        today = date.today()
+
+        if self.request.user.is_staff:
+            return super().get_queryset()
+
+        else:
+            queryset = Flight.objects.filter(date_of_departure__gt=today)
+
+            if Flight.objects.filter(date_of_departure=today):
+                today_queryset = Flight.objects.filter(
+                    estimated_time_of_departure__gt=current_time)
+                queryset = queryset.union(today_queryset)
+                return queryset
 
     def get_serializer_class(self):
         if self.request.user.is_staff:
